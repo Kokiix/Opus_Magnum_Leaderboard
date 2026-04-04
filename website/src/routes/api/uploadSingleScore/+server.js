622782d5d41d21b5b711fdfa67264d971b12cea7
kenneth <kokiidoki@gmail.com>
2026-04-04 12:46:58 -0400

    Fix using row object directly instead of property

--- website/src/routes/api/uploadSingleScore/+server.js
+++ website/src/routes/api/uploadSingleScore/+server.js
@@ -4,28 +4,28 @@ export const POST = async ({ request }) => {
     // TODO: move to env file
     if (request.headers.get("very_secret_key") != "QCR8VE5UNSo6XHVOa11rX0A1eXxJQW5ubkBRLWEjLS9tSHNuLjx0XC4nLEYrLTo=")
         return new Response(null, { status: 404 });
-    const new_score = await request.json();
+    const newScore = await request.json();
     const supabase = createClient( // TODO: change url when going public, move to env file
         'https://zeddvrudhdrakfbmzinh.supabase.co',
         'sb_publishable_coM9-yUpcpkfpQfBf7y6Ug_HdbicKL3');
 
     // Check for existence
-    const { data: existingSum, error } = await supabase
+    const { data: existingScore, error } = await supabase
         .from('scores')
         .select('sum')
-        .eq('level', new_score.level)
-        .eq('steam_id', new_score.steam_id)
+        .eq('level', newScore.level)
+        .eq('steam_id', newScore.steam_id)
         .maybeSingle();
 
     if (error)
         return new Response(`Failed to get existing score: ${error.message}`, { status: 500 });
 
     // Insert
-    console.log(existingSum);
-    if (!existingSum || new_score.sum < existingSum) {
+    console.log(existingScore);
+    if (!existingScore || newScore.sum < existingScore.sum) {
         const { error } = await supabase
             .from('scores')
-            .insert(new_score);
+            .insert(newScore);
         if (error)
             return new Response(`Failed to insert new score: ${error.message}`, { status: 500 });
         return new Response(null, { status: 201 });
