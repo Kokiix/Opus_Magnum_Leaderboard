6afd79810a688b4c5067833ec8c2fac121c29866
kenneth <kokiidoki@gmail.com>
2026-04-13 17:24:02 -0400

    Add tab title "opus magnumoki leaderboard"

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Opus Magnumoki Leaderboard</title>
 </svelte:head>
 
 {@render children()}
